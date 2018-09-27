2f691c27bfd6e7d42c4af385311773a4b675fc77
Andrew Bernier <a6bernier@gmail.com>
2018-09-27 19:20:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if board[index].include?(0..8) 
+  if board[index].include?(0..8)
     true
-  elsif 
+  elsif
     false
   end
 end
