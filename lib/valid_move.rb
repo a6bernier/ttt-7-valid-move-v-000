fc8a241c5082ea9ac851bc89f6d80dd927be10d4
Andrew Bernier <a6bernier@gmail.com>
2018-09-27 19:42:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if index != 0..8 && position_taken?(board, index) == true
     false
-  else
+  elsif elsif position_taken?(board, index) == false
     true
   end
 end
